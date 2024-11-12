2313fcde1e4dfad544fdf53528cc1a0578879b1e
MaribelVilca <maribelvilcatincopa3@gmail.com>
2024-11-12 08:34:37 -0500

    c1

--- config/config1.php
+++ config/config1.php
@@ -1,8 +1,8 @@
 <?php
 
 const BD_HOST ='localhost';
-const BD_NAME ='sistema_ventas';
-const BD_USER ='root';
+const BD_NAME ='';
+const BD_USER ='';
 const BD_PASSWORD ='';
 
 const BD_CHARSET ='';
