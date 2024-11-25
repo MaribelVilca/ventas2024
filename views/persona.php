cb90ea418b0e4da829cf4c7acc39b0292bed9274
MaribelVilca <maribelvilcatincopa3@gmail.com>
2024-11-25 14:53:30 -0500

    4444

---
+++ views/persona.php
@@ -0,0 +1,27 @@
+<div class="col_12">
+    <table border="1" style="width: 100%;"cellspacing>
+        <thead>
+          <tr>
+            <th>Nro_identidad</th>
+            <th>Razon_social</th>
+            <th>telefono</th>
+            <th>correo</th>
+            <th>departamento</th>
+            <th>provincia</th>
+            <th>distrito</th>
+            <th>cod_postal</th>
+            <th>direccion</th>
+            <th>rol</th>
+            <th>password</th>
+            <th>estado</th>
+            <th>fecha_reg</th>
+
+          </tr>  
+
+        </thead>
+        <tbody id="tbl_persona">
+     
+     </tbody>
+    </table>
+</div>
+<script src="<?php echo BASE_URL; ?>views/js/functions_persona.js"></script>
